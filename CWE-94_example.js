ca33540f5e2f045325e3e7f8f195597ea74a906f
Rock Li <76486033+RokkuRock@users.noreply.github.com>
2025-06-26 09:38:49 +0800

    Update CWE-94_example.js

--- CWE-94_example.js
+++ CWE-94_example.js
@@ -1,6 +1,6 @@
-// codeInject.js
-const rl = require('readline').createInterface(process.stdin, process.stdout);
-rl.question('Enter JS: ', code => {
-  console.log(eval(code)); // CWE-94 :contentReference[oaicite:12]{index=12}
+// codeInjectVm.js
+const vm = require('vm'), rl = require('readline').createInterface(process.stdin, process.stdout);
+rl.question('Enter JS code: ', code => {
+  vm.runInThisContext(code); // CWE‑94
   rl.close();
 });
