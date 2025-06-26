735fc00bbfd2fee3b2c39d9b6caac17df85d11be
Rock Li <76486033+RokkuRock@users.noreply.github.com>
2025-06-26 17:26:14 +0800

    Delete CWE-94_example.js

--- CWE-94_example.js
+++
@@ -1,7 +0,0 @@
-// sandboxInject.js
-const vm = require('vm');
-const readline = require('readline').createInterface(process.stdin, process.stdout);
-readline.question('Code to execute: ', code => {
-  vm.runInNewContext(code, {}); // CWE-94
-  readline.close();
-});
