48bebc3771f0c76b832e587aee5f94e6cb803901
Rock Li <76486033+RokkuRock@users.noreply.github.com>
2025-06-26 09:34:31 +0800

    Create CWE-502_example.js

---
+++ CWE-502_example.js
@@ -0,0 +1,11 @@
+// deserialize.js
+const http = require('http');
+http.createServer((req, res) => {
+  let body = '';
+  req.on('data', b => body += b);
+  req.on('end', () => {
+    const obj = JSON.parse(body); // CWE-502
+    console.log(obj);
+    res.end('OK');
+  });
+}).listen(9000);
