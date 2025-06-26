fa6edf166fe43bfdefc92920bbee8129abd4955f
Rock Li <76486033+RokkuRock@users.noreply.github.com>
2025-06-26 10:30:23 +0800

    Update CWE-78_example.js

--- CWE-78_example.js
+++ CWE-78_example.js
@@ -1,8 +1,12 @@
-// cmdInject2.js
-const { spawnSync } = require('child_process');
-const readline = require('readline').createInterface(process.stdin, process.stdout);
-readline.question('File to run: ', f => {
-  const res = spawnSync('sh', ['-c', f]); // CWE-78
-  console.log(res.stdout.toString());
-  readline.close();
+// cmdInject.js
+const { execFile } = require('child_process');
+const rl = require('readline').createInterface(process.stdin, process.stdout);
+
+rl.question('Enter script name in ./scripts/: ', name => {
+  // CWE-78: 未驗證 name，可注入 shell 參數
+  execFile('bash', ['-c', `./scripts/${name}`], (err, stdout) => {
+    if (err) console.error(err);
+    else console.log(stdout);
+    rl.close();
+  });
 });
