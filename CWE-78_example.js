c2da4ffad705bad296557efa9e4db1e9cf53cf48
Rock Li <76486033+RokkuRock@users.noreply.github.com>
2025-06-26 12:23:44 +0800

    Update CWE-78_example.js

--- CWE-78_example.js
+++ CWE-78_example.js
@@ -1,12 +1,12 @@
-// cmdInject.js
-const { execFile } = require('child_process');
+// cmd_inject.js
+const { exec } = require('child_process');
 const rl = require('readline').createInterface(process.stdin, process.stdout);
 
-rl.question('Enter script name in ./scripts/: ', name => {
-  // CWE-78: 未驗證 name，可注入 shell 參數
-  execFile('bash', ['-c', `./scripts/${name}`], (err, stdout) => {
+rl.question('Enter filename to list: ', fn => {
+  // CWE-78: 未驗證 fn，可注入如 "; rm -rf /tmp/x"
+  exec(`ls ${fn}`, (err, out) => {
     if (err) console.error(err);
-    else console.log(stdout);
+    else console.log(out);
     rl.close();
   });
 });
