52ca6a141e67964694e3ccf49dd7d2990ca49373
Rock Li <76486033+RokkuRock@users.noreply.github.com>
2025-06-26 17:27:45 +0800

    Delete CWE-77_example.js

--- CWE-77_example.js
+++
@@ -1,8 +0,0 @@
-// cmdInject.js
-const { execFile } = require('child_process'), rl = require('readline').createInterface(process.stdin, process.stdout);
-rl.question('Script name: ', script => {
-  execFile('/bin/sh', ['-c', `./scripts/${script}`], (err, stdout) => {
-    console.log(stdout);
-  }); // CWE-77: user 控制 script 參數
-  rl.close();
-});
