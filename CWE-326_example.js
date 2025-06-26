aea3e0e04e31ec898b5c396a7a4962e66a1f5650
Rock Li <76486033+RokkuRock@users.noreply.github.com>
2025-06-26 10:25:17 +0800

    Update CWE-326_example.js

--- CWE-326_example.js
+++ CWE-326_example.js
@@ -1,7 +1,7 @@
-// weakCrypto.js
+// weakcrypto2.js
 const crypto = require('crypto');
-const key = Buffer.alloc(16, 'A');
-const iv = Buffer.alloc(16, 0); // CWE‑326 (fixed IV)
-const cipher = crypto.createCipheriv('aes-128-cbc', key, iv);
-let out = cipher.update('secret', 'utf8', 'hex') + cipher.final('hex');
+const key = Buffer.from('0123456789abcdef0123456789abcdef', 'hex');
+const iv = Buffer.alloc(16, 0); // CWE-326: 固定 IV
+const cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
+let out = cipher.update('secret data', 'utf8', 'hex') + cipher.final('hex');
 console.log(out);
