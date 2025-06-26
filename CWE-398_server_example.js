ca6be19103022278abaad5bcc21e4525deec70ea
Rock Li <76486033+RokkuRock@users.noreply.github.com>
2025-06-26 17:00:24 +0800

    Delete CWE-398_server_example.js

--- CWE-398_server_example.js
+++
@@ -1,7 +0,0 @@
-// crashLoop.js
-const rl = require('readline').createInterface(process.stdin, process.stdout);
-rl.question('Loop count: ', n => {
-  for (let i = 0; i < Number(n); i++) { /* empty */ }
-  console.log('Done');
-  rl.close(); // CWE-398: 資源耗盡風險
-});
