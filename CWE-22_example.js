09e55df00863b7bc615cf022aabe1859ca39eeaf
Rock Li <76486033+RokkuRock@users.noreply.github.com>
2025-06-26 12:24:38 +0800

    Update CWE-22_example.js

--- CWE-22_example.js
+++ CWE-22_example.js
@@ -1,10 +1,15 @@
-// pathTraversal.js
+// path_traversal.js
 const http = require('http');
-const fs = require('fs');
+const fs   = require('fs');
 
 http.createServer((req, res) => {
   const file = new URL(req.url, 'http://a').searchParams.get('file');
-  // CWE-22: 未過濾 ../
-  let stream = fs.createReadStream(`data/${file}`);
-  stream.pipe(res);
-}).listen(3002);
+  // CWE-22: 未過濾 "../" 可讀取任意檔案
+  try {
+    const data = fs.readFileSync(`storage/${file}`, 'utf8');
+    res.end(data);
+  } catch {
+    res.statusCode = 404;
+    res.end('Not found');
+  }
+}).listen(3000);
