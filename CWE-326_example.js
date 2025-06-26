bc5b3cda92e7a2feee3d25146b26e73afb629c6f
Rock Li <76486033+RokkuRock@users.noreply.github.com>
2025-06-26 17:07:44 +0800

    Delete CWE-326_example.js

--- CWE-326_example.js
+++
@@ -1,6 +0,0 @@
-// weak_crypto.js
-const crypto = require('crypto');
-const key = 'hardcodedkey';
-const hmac = crypto.createHmac('md5', key); // CWE-326
-hmac.update('sensitive-data');
-console.log(hmac.digest('hex'));
