26fe899870df4b2a9766259e576e9bf8805ec2ed
Rock Li <76486033+RokkuRock@users.noreply.github.com>
2025-06-26 09:57:25 +0800

    Update CWE-918_example.js

--- CWE-918_example.js
+++ CWE-918_example.js
@@ -1,6 +1,6 @@
-// ssrf.js
+// ssrf-new.js
 const http = require('http');
 const url = process.argv[2];
-http.get(url, resp => {
-  resp.on('data', d => process.stdout.write(d));
-}); // CWE-918
+http.get(url, res => {
+  res.pipe(process.stdout);
+}); // CWE-918: 不驗證任意 URL 即發請求 :contentReference[oaicite:9]{index=9}
