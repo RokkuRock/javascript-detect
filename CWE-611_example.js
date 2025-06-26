9587ae99a92f3094fd0afc02865834782c18139b
Rock Li <76486033+RokkuRock@users.noreply.github.com>
2025-06-26 17:20:28 +0800

    Delete CWE-611_example.js

--- CWE-611_example.js
+++
@@ -1,6 +0,0 @@
-// xxe2.js
-const fs = require('fs');
-const { DOMParser } = require('@xmldom/xmldom');
-const xml = fs.readFileSync('user.xml', 'utf8');
-const doc = new DOMParser().parseFromString(xml, 'application/xml'); // CWE-611
-console.log(doc.documentElement.tagName);
