2e0db9783a5db6c8ace845afd7ac03f4a6dea4a1
Rock Li <76486033+RokkuRock@users.noreply.github.com>
2025-06-26 12:25:11 +0800

    Update CWE-326_example.js

--- CWE-326_example.js
+++ CWE-326_example.js
@@ -1,7 +1,6 @@
-// weakcrypto2.js
+// weak_crypto.js
 const crypto = require('crypto');
-const key = Buffer.from('0123456789abcdef0123456789abcdef', 'hex');
-const iv = Buffer.alloc(16, 0); // CWE-326: 固定 IV
-const cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
-let out = cipher.update('secret data', 'utf8', 'hex') + cipher.final('hex');
-console.log(out);
+const key = 'hardcodedkey';
+const hmac = crypto.createHmac('md5', key); // CWE-326
+hmac.update('sensitive-data');
+console.log(hmac.digest('hex'));
