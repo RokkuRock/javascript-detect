e7e512366486ad47198cc742879db3cd2780283e
Rock Li <76486033+RokkuRock@users.noreply.github.com>
2025-06-26 17:01:48 +0800

    Delete CWE-494_example.js

--- CWE-494_example.js
+++
@@ -1,7 +0,0 @@
-// insecureDownload.js
-const fs = require('fs'), http = require('http');
-const url = process.argv[2];
-http.get(url, res => {
-  const file = fs.createWriteStream("downloaded.bin");
-  res.pipe(file);
-}); // CWE-494: 使用者控制 download URL
