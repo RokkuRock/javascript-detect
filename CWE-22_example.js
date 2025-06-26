0b285cc21b1e04b0ff1c256048738a274fa7f27d
Rock Li <76486033+RokkuRock@users.noreply.github.com>
2025-06-26 09:38:13 +0800

    Update CWE-22_example.js

--- CWE-22_example.js
+++ CWE-22_example.js
@@ -1,11 +1,9 @@
-// File: pathTraversal.js
-const fs = require('fs');
-const readline = require('readline');
-const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
-rl.question('Which file in safe/ to load: ', (fn) => {
-  fs.readFile(`safe/${fn}`, 'utf8', (e, d) => { // CWE‑22
-    if (e) return console.error(e);
-    console.log(d);
+// zipSlip.js
+const fs = require('fs'), unzipper = require('unzipper');
+const zipPath = process.argv[2];
+fs.createReadStream(zipPath)
+  .pipe(unzipper.Parse())
+  .on('entry', entry => {
+    const filePath = "out/" + entry.path; // CWE-22: 未檢查 '../'
+    entry.pipe(fs.createWriteStream(filePath));
   });
-  rl.close();
-});
