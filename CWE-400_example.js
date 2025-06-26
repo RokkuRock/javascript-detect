de8b269746aed6f669d6fc0482a1bfc36a96539a
Rock Li <76486033+RokkuRock@users.noreply.github.com>
2025-06-26 21:31:19 +0800

    Update CWE-400_example.js

--- CWE-400_example.js
+++ CWE-400_example.js
@@ -1,9 +1,6 @@
-// regexDos.js
 const http = require('http');
 http.createServer((req, res) => {
-  // 用戶輸入決定正則，可能導致 ReDoS
-  const pattern = new RegExp(req.url.slice(1)); // CWE-400
-  // 不斷測試以放大資源耗盡
+  const pattern = new RegExp(req.url.slice(1)); 
   for (let i = 0; i < 100000; i++) pattern.test('aaaaaaaaaaaaaaaaaaaa!');
   res.end('Done');
 }).listen(3000);
