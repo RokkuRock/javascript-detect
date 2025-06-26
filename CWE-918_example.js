bc4e0f49380472068480cca4ec8057307584d45e
Rock Li <76486033+RokkuRock@users.noreply.github.com>
2025-06-26 17:20:50 +0800

    Delete CWE-918_example.js

--- CWE-918_example.js
+++
@@ -1,9 +0,0 @@
-// ssrf2.js
-const http = require('http');
-const rl = require('readline').createInterface(process.stdin, process.stdout);
-
-rl.question('Enter URL: ', url => {
-  // CWE-918: 任意 URL 請求
-  http.get(url, res => res.pipe(process.stdout));
-  rl.close();
-});
