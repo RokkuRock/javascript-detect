63368cb1f28ada6b906e85e5f1254fe890087fbd
Rock Li <76486033+RokkuRock@users.noreply.github.com>
2025-06-26 21:31:30 +0800

    Update CWE-444_example.js

--- CWE-444_example.js
+++ CWE-444_example.js
@@ -1,7 +1,5 @@
-// insecureCookie.js
 const http = require('http');
 http.createServer((req, res) => {
-  // CWE-444: 未設定 Secure/HttpOnly，cookie 易被盜
   res.setHeader('Set-Cookie', 'sessionId=abc123; Path=/');
   res.end('Cookie set');
 }).listen(3008, () => console.log('Cookie server on 3008'));
