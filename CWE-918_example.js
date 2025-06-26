0f9f45ad6e6af2d626f572056fb0f4cd18257f6a
Rock Li <76486033+RokkuRock@users.noreply.github.com>
2025-06-26 12:24:55 +0800

    Update CWE-918_example.js

--- CWE-918_example.js
+++ CWE-918_example.js
@@ -3,7 +3,7 @@ const http = require('http');
 const rl = require('readline').createInterface(process.stdin, process.stdout);
 
 rl.question('Enter URL to fetch: ', url => {
-  // CWE-918: 任意 URL
+  // CWE-918: 任意 URL，可用於內網掃描
   http.get(url, r => r.pipe(process.stdout));
   rl.close();
 });
