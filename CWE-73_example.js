0d40e7f8640c58a62458b7b70042d6bdf15d5985
Rock Li <76486033+RokkuRock@users.noreply.github.com>
2025-06-26 17:11:06 +0800

    Delete CWE-73_example.js

--- CWE-73_example.js
+++
@@ -1,8 +0,0 @@
-// pathInject.js
-const fs = require('fs');
-const rl = require('readline').createInterface(process.stdin, process.stdout);
-rl.question('Filename in safe/: ', fn => {
-  const data = fs.readFileSync('safe/' + fn, 'utf8'); // CWE-73
-  console.log(data);
-  rl.close();
-});
