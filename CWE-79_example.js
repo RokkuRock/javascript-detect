4c0aa453c376aef9aff1a22ae33b37d712304d47
Rock Li <76486033+RokkuRock@users.noreply.github.com>
2025-06-26 09:33:07 +0800

    Update CWE-79_example.js

--- CWE-79_example.js
+++ CWE-79_example.js
@@ -1,7 +1,7 @@
-// File: xss.js
+// xss.js
 const http = require('http');
 http.createServer((req, res) => {
-  const msg = new URL(req.url, 'http://localhost').searchParams.get('msg') || '';
+  const msg = new URL(req.url, 'http://a').searchParams.get('msg') || '';
   res.writeHead(200, {'Content-Type':'text/html'});
-  res.end(`<h1>You said: ${msg}</h1>`); // CWE-79
-}).listen(8080, () => console.log('Server at http://localhost:8080/?msg=...'));
+  res.end(`<div>${msg}</div>`); // CWE-79
+}).listen(8080);
