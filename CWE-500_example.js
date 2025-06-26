e5568a80497c93bb58d5560430fcd826944ac608
Rock Li <76486033+RokkuRock@users.noreply.github.com>
2025-06-26 21:31:43 +0800

    Update CWE-500_example.js

--- CWE-500_example.js
+++ CWE-500_example.js
@@ -1,9 +1,8 @@
-// ssti.js
 const http = require('http');
 http.createServer((req, res) => {
   if (req.url.startsWith('/show?msg=')) {
     const msg = req.url.split('=')[1];
-    const tmpl = new Function('return `' + msg + '`;'); // CWE-500
+    const tmpl = new Function('return `' + msg + '`;');
     res.end(tmpl());
   }
 }).listen(3002);
