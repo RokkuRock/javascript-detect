7c734e3ef9332a27008fa50ca7cfda1222ea99f9
Rock Li <76486033+RokkuRock@users.noreply.github.com>
2025-06-26 17:06:38 +0800

    Delete CWE-116_example.js

--- CWE-116_example.js
+++
@@ -1,7 +0,0 @@
-// respSplitting.js
-const http = require('http');
-http.createServer((req, res) => {
-  const url = new URL(req.url, 'http://a').searchParams.get('u');
-  res.writeHead(200, { 'Set-Cookie': 'id=' + url }); // CWE-116
-  res.end('Done');
-}).listen(3001);
